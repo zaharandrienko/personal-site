cd004085731c60d0faee94359ec98193cd3b48b3
Zahar Andrienko <zaharandrienko88@gmail.com>
2018-09-05 21:13:45 +0300

    betta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   {path: "", redirectTo: "/home", pathMatch: "full"},
   {path: "home", component: HomeComponent},
   {path: "about", component: AboutComponent},
-  {path: "contact", component: ContactComponent},
+  // {path: "contact", component: ContactComponent},
   {path: "news" , component: NewsComponent}
 ];
 
